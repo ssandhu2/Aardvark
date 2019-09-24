72cc5ba2fb5e643bd16a32108a63e45571c33660
Alan Nguyene <nguyen.alan89@gmail.com>
2019-09-24 14:42:13 -0700

    fine tuned everyones about page, added comments so its easier to read in server.js and some in about.html

--- application/Server.js
+++ application/Server.js
@@ -19,48 +19,56 @@ router.get('/style', function(req, res) {
 router.get("/about",function(req,res){
   res.sendFile(path + "about.html");
 });
+//Alan's
 router.get("/aboutAlan",function(req,res){
   res.sendFile(path + "aboutAlan.html");
 });
 router.get("/alanjpg", function (req, res) {
   res.sendFile(path + "images/alan.jpg");
 });
+//Daisy's
 router.get("/aboutDaisy",function(req,res){
   res.sendFile(path + "aboutDaisy.html");
 });
-router.get("/daisyjpg", function (req, res) {
-  res.sendFile(path + "images/Daisy.jpg");
+router.get("/daisypng", function (req, res) {
+  res.sendFile(path + "images/Daisy.png");
 });
+//Jon's
 router.get("/aboutJonathan",function(req,res){
   res.sendFile(path + "aboutJonathan.html");
 });
 router.get("/BigJonjpg", function (req, res) {
   res.sendFile(path + "images/BigJon.jpg");
 });
+//Russell's
 router.get("/aboutRussell", function (req, res) {
   res.sendFile(path + "aboutRussell.html");
 });
 router.get("/russelljpg", function (req, res) {
   res.sendFile(path + "images/russell.jpg");
 });
+//Ida's
 router.get("/aboutIda", function (req, res) {
   res.sendFile(path + "aboutIda.html");
 });
 router.get("/idajpg", function (req, res) {
   res.sendFile(path + "images/ida.jpg");
 });
+//Sunny's
 router.get("/aboutSunminder", function (req, res) {
   res.sendFile(path + "aboutSunminder.html");
 });
 router.get("/Sunminderjpg", function (req, res) {
   res.sendFile(path + "images/Sunminder.jpg");
 });
+//Ryan's
 router.get("/aboutRyan", function (req, res) {
   res.sendFile(path + "aboutRyan.html");
 });
 router.get("/ryanjpg", function (req, res) {
   res.sendFile(path + "images/r.jpg");
 });
+
 router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
