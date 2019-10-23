9b0dd9df2f03a28802151fba62359439d8f395b3
russ3llwong <russellwong97@hotmail.com>
2019-10-22 18:51:14 -0700

    Polished search logic to account for approved items. Pass in null data if GET request. Corrected search bar dropdown values.

--- application/routes/itemRoutes.js
+++ application/routes/itemRoutes.js
@@ -1,43 +1,52 @@
 const express = require ('express');
 const sqlRouter = express.Router();
-const https = require('https');
 const db = require('../model/db.js');
 const bodyparser = require('body-parser');
 const init = require('../model/init.js');
 
+// parser to parse request body form-data
 let parser = bodyparser.urlencoded({extended: false});
+
 let app = express();
 app.use(parser);
 
+// if go straight to searchResults page via URL, no data is passed onto views
 sqlRouter.get("/", (req, res) => {
-    //TO ADD CODE HERE IF SOMEONE GOES STRAIGHT TO THE URL
-    res.render("results",);
+    res.render("results", {
+        searchTerm: "",
+        searchResults: "",
+        type: ""
+    })
 });
 
+// search bar action type is POST
 sqlRouter.post("/", parser, (req,res) => {
 
-    //get request body stuff from index.ejs 
+    // get request body form-data from index.ejs 
     let searchTerm = req.body.search;
     let type = req.body.type;
-    //search logic
+
+    // search logic
+    // status=1 for approved items
     let query = "SELECT * FROM item;";
     if (searchTerm != '' && type != ''){
-        query = `SELECT * FROM item WHERE type="${type}" AND ( name LIKE "%${searchTerm}%" );`
+        query = `SELECT * FROM item WHERE status=1 AND type="${type}" AND ( name LIKE "%${searchTerm}%" OR description LIKE "%${searchTerm}%");`
     }
     else if (searchTerm != '' && type == ''){
-        query = `SELECT * FROM item WHERE name LIKE "%${searchTerm}%";`
+        query = `SELECT * FROM item WHERE status=1 AND name LIKE "%${searchTerm}%" OR description LIKE "%${searchTerm}%";`
     }
     else if (searchTerm == '' && type != ''){
-        query = `SELECT * FROM item WHERE type="${type}";`
+        query = `SELECT * FROM item WHERE status=1 AND type="${type}";`
     }
 
-    //print db query before making the query
+    // print db query for debugging purposes
     console.log(query);
 
-    //db query to get results
+    // db query to get results
     db.query(query, (err, result) => {
         if (err) {
             console.log(err);
+            // data is null in case of error
             req.searchResult = "";
             req.searchTerm = "";
             req.type = "";
@@ -47,15 +56,14 @@ sqlRouter.post("/", parser, (req,res) => {
         req.searchTerm = searchTerm;
         req.type = type;
 
+        // print results for debugging purposes
         console.log(`searchTerm: ${searchTerm}, type: ${type}`);
-
-        //this prints the items fetched from db if any
         console.log(result);
 
-        //these are what passed into results.ejs
-        //searchTerm for what was typed into the search bar
-        //type for the type selected, null if All Types
-        //searchResults is the array of items. 
+        // these are what passed into results.ejs
+        // searchTerm for what was typed into the search bar
+        // type for the type selected, null if All Types
+        // searchResults is the array of items. 
         res.render("results", {
             searchTerm: req.searchTerm,
             searchResults: req.searchResult,
@@ -64,6 +72,7 @@ sqlRouter.post("/", parser, (req,res) => {
     })
 })
 
+// for testing
 sqlRouter.route('/tables').get((req, res) => {
     (async () => {
       await init.CreateTables();
@@ -71,7 +80,8 @@ sqlRouter.route('/tables').get((req, res) => {
     })();
   });
 
-  sqlRouter.route('/insert').get((req, res) => {
+// for testing
+ sqlRouter.route('/insert').get((req, res) => {
     (async () => {
       await init.InsertDummy();
       res.send('Inserted Data');
