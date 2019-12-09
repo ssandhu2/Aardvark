e6c50d9d7deb7aaeb65d0214614a8d71a2e625df
Ryan <ryanshu78@gmail.com>
2019-12-08 21:20:08 -0800

    Finished adding login status to all pages

--- application/routes/aboutRoutes.js
+++ application/routes/aboutRoutes.js
@@ -2,35 +2,49 @@ let express = require('express');
 let about_routes = express.Router();
 
 about_routes.get("/ida", function (req, res) {
-    res.render("aboutIda", {page: 'about'});
+    res.render("aboutIda", 
+    {page: 'about',
+    loggedin: req.user});
 });
 
 about_routes.get("/alan", function (req, res) {
-    res.render("aboutAlan", {page: 'about'});
+    res.render("aboutAlan", 
+    {page: 'about',
+    loggedin: req.user});
 });
 
 about_routes.get("/russell", function (req, res) {
-    res.render("aboutRussell", {page: 'about'});
+    res.render("aboutRussell", 
+    {page: 'about',
+    loggedin: req.user});
 });
 
 about_routes.get("/ryan", function (req, res) {
-    res.render("aboutRyan", {page: 'about'});
+    res.render("aboutRyan", 
+    {page: 'about',
+    loggedin: req.user});
 });
 
 about_routes.get("/jonathan", function (req, res) {
-    res.render("aboutJonathan", {page: 'about'});
+    res.render("aboutJonathan", 
+    {page: 'about',
+    loggedin: req.user});
 });
 
 about_routes.get("/daisy", function (req, res) {
-    res.render("aboutDaisy", {page: 'about'});
+    res.render("aboutDaisy", 
+    {page: 'about',
+    loggedin: req.user});
 });
 
 about_routes.get("/sunminder", function (req, res) {
-    res.render("aboutSunminder", {page: 'about'});
+    res.render("aboutSunminder", 
+    {page: 'about',
+    loggedin: req.user});
 });
-
 about_routes.route('/').get((req,res) => {
-    res.render('about', {page: 'about'});
+    res.render('about', 
+    {page: 'about',
+    loggedin: req.user});
 });
-
 module.exports = about_routes;
--- application/routes/dashboardRoutes.js
+++ application/routes/dashboardRoutes.js
@@ -3,7 +3,7 @@ const dashboardRouter = express.Router();
 const db = require('../model/db.js');
 const bodyparser = require('body-parser');
 const passport = require('passport');
-const { loggedIn, isLoggedIn } = require('../model/validator.js'); // to check if user is logged in 
+const { loggedIn } = require('../model/validator.js'); // to check if user is logged in 
 
 // parser to parse request body form-data
 let parser = bodyparser.urlencoded({ extended: false });
